36c6be416fa48f362ce1007db5e60c6d583b2a44
Adam Williamson <Adam.Williamson@finance-ni.gov.uk>
2023-09-06 12:06:00 +0100

    Coding standards

--- web/modules/custom/corplite_migrations/src/Plugin/migrate/process/StripHTMLFilter.php
+++ web/modules/custom/corplite_migrations/src/Plugin/migrate/process/StripHTMLFilter.php
@@ -2,8 +2,8 @@
 
 namespace Drupal\corplite_migrations\Plugin\migrate\process;
 
-use Drupal\migrate\ProcessPluginBase;
 use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\ProcessPluginBase;
 use Drupal\migrate\Row;
 
 /**
