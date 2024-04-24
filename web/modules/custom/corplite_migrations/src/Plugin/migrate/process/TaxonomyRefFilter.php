b949afce3c92bf5f559c23d93ab8cf3e4089ec8e
DuttonMa <duttonma@gmail.com>
2024-04-24 15:30:50 +0100

    taxonomy_ref_filter plugin working

--- web/modules/custom/corplite_migrations/src/Plugin/migrate/process/TaxonomyRefFilter.php
+++ web/modules/custom/corplite_migrations/src/Plugin/migrate/process/TaxonomyRefFilter.php
@@ -22,17 +22,11 @@ class TaxonomyRefFilter extends ProcessPluginBase {
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
     $matches = [];
     // Look for all references to taxonomy terms in the text.
-    if (preg_match_all('|\<a href=\"\/taxonomy\/term\/(\d*)"\>([^\<]*)\<\/a\>|', $value, $matches)) {
-      if (count($matches) > 0) {
-        object_log('matches', $matches);
-        object_log('matches[0]', $matches[0]);
-        object_log('matches[0][0]', $matches[0][0]);
-        \Drupal::logger('topic_migration')->notice(t('Starting loop'));
-        foreach ($matches[0] as $this_link) {
-          object_log('this_link', $this_link);
-          $msg = "Link is " . $this_link;
-          \Drupal::logger('topic_migration')->notice(t($msg));
-          //$value['value'] = str_replace($this->configuration['from_ref'], $this->configuration['to_ref'], $value['value']);
+    if (preg_match_all('|\<a href=\"\/taxonomy\/term\/(\d*)"\>[^\<]*\<\/a\>|', $value, $matches)) {
+      if (count($matches) > 1) {
+        foreach ($matches[1] as $this_link) {
+          // Surround the tid with '@@' characters so that it can be updated later.
+          $value = str_replace('/taxonomy/term/' . $this_link, '/taxonomy/term/@@' . $this_link . '@@', $value);
         }
       }
     }
