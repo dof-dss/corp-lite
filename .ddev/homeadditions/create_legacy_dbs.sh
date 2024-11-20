d3b84598cb06d37811cd301f83336e0a376316aa
DuttonMa <duttonma@gmail.com>
2024-11-20 16:59:55 +0000

    Tweaks to legacy db creation

--- .ddev/homeadditions/create_legacy_dbs.sh
+++ .ddev/homeadditions/create_legacy_dbs.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-. /helpers/log.sh
-
 # Iterate every symlink (which web create for each site) under web/sites and create a database with the default
 # Lando database credentials if it doesn't already exist.
+echo "Creating legacy databases"
 for dir in $(find /var/www/html/web/sites/ -mindepth 1 -maxdepth 1 -type l) ; do
   database=${dir##*/} ;
 
@@ -11,6 +10,6 @@ for dir in $(find /var/www/html/web/sites/ -mindepth 1 -maxdepth 1 -type l) ; do
   if [ $database != 'default' ]; then
      # Create the Drupal 7 legacy databases if they don't already exist.
      mysql -uroot -proot -e "CREATE DATABASE IF NOT EXISTS ${database}_legacy; \
-                      GRANT ALL PRIVILEGES ON ${database}_legacy.* TO 'db'@'localhost' IDENTIFIED by 'db';" | cut -f1 -d":";
+                      GRANT ALL PRIVILEGES ON ${database}_legacy.* TO '$DB_USER'@'localhost' IDENTIFIED by '$DB_PASS';" | cut -f1 -d":";
   fi
 done
