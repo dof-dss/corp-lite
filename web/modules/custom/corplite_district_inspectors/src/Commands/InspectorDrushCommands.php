b4b2d044b79c12b1ae26db80c54ee80722eb55b7
DuttonMa <duttonma@gmail.com>
2025-01-07 13:06:52 +0000

    Tidy up schools index command

--- web/modules/custom/corplite_district_inspectors/src/Commands/InspectorDrushCommands.php
+++ web/modules/custom/corplite_district_inspectors/src/Commands/InspectorDrushCommands.php
@@ -27,31 +27,20 @@ class InspectorDrushCommands extends DrushCommands {
   }
 
   /**
-   * Drush command to update all school and inspector
+   * Drush command to update all school
    * entities to make them available to Solr.
    *
-   * @command index-inspectors
+   * @command index-schools
    */
-  public function indexInspectors() {
-    $this->io()->write("About to run query", TRUE);
+  public function indexSchools() {
     $database = \Drupal::database();
     $query = $database->query("SELECT * FROM school");
     $result = $query->fetchAll();
     foreach ($result as $school_row) {
-      $this->io()->write("School id is " . $school_row->id, TRUE);
       $school = School::load($school_row->id);
-      //$this->io()->write("Revision created " . $school->get('revision_created')->getString(), TRUE);
       $school->save();
     }
-    $query = $database->query("SELECT * FROM inspector");
-    $result = $query->fetchAll();
-    foreach ($result as $inspector_row) {
-      $this->io()->write("Inspector id is " . $inspector_row->id, TRUE);
-      $inspector = Inspector::load($inspector_row->id);
-      //$this->io()->write("Revision created " . $school->get('revision_created')->getString(), TRUE);
-      $inspector->save();
-    }
-    $this->io()->write("Successfully ran query", TRUE);
+    $this->io()->write("Schools indexed", TRUE);
   }
 
 }
