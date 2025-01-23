61e874475631e736dc9b372771379213d4ecb397
DuttonMa <duttonma@gmail.com>
2025-01-23 10:27:42 +0000

    Code improvements

--- web/modules/custom/corplite_district_inspectors/js/auto_submit.js
+++ web/modules/custom/corplite_district_inspectors/js/auto_submit.js
@@ -1,5 +1,6 @@
 (function ($) {
   "use strict";
+  /* Submit the district inspectors form when the user clicks on a typeahead option */
   Drupal.behaviors.autosubmit = {
     attach: function (context) {
       $('input[name="search_api_fulltext"]').on('autocompleteclose', function (event, data) {
--- web/modules/custom/corplite_district_inspectors/src/Controller/AutoCompleteController.php
+++ web/modules/custom/corplite_district_inspectors/src/Controller/AutoCompleteController.php
@@ -9,33 +9,44 @@ use Symfony\Component\HttpFoundation\Request;
 class AutoCompleteController extends ControllerBase {
   public function handleAutoComplete(Request $request) {
     $matches = [];
+    // Retrieve the string that the user has typed.
     $string = $request->query->get('q');
     if (strlen($string) >= 3) {
-      $index = \Drupal\search_api\Entity\Index::load('school');
-      $query = $index->query();
-      $query->keys($string);
-      $query->setFulltextFields(['name', 'de_reference']);
-      $results = $query->execute();
-      if ($results->getResultCount() > 0) {
-        foreach ($results->getResultItems() as $result) {
-          $school_name = $result->getField('name')->getValues();
-          $de_ref = $result->getField('de_reference')->getValues();
-          if (is_numeric($string)) {
-            $matches[] = [
-              'value' => (string)$de_ref[0]
-            ];
-          } else {
-            $matches[] = [
-              'value' => (string)$school_name[0]
-            ];
-          }
-        }
-      } else {
-        $matches[] = [
-          'value' => $this->t("No schools found")
-        ];
-      }
+      // Fire the autocomplete when they have typed at least 3 chars.
+      $matches = getMatches($string);
     }
     return new JsonResponse($matches);
+  }
+
+  private function getMatches($string) {
+    // Retrieve matches from Solr.
+    $index = \Drupal\search_api\Entity\Index::load('school');
+    $query = $index->query();
+    $query->keys($string);
+    $query->setFulltextFields(['name', 'de_reference']);
+    $results = $query->execute();
+    // How many results have we retrieved ?
+    if ($results->getResultCount() > 0) {
+      foreach ($results->getResultItems() as $result) {
+        $school_name = $result->getField('name')->getValues();
+        $de_ref = $result->getField('de_reference')->getValues();
+        if (is_numeric($string)) {
+          // If the user typed a number, return DE references in the autocomplete.
+          $matches[] = [
+            'value' => (string)$de_ref[0]
+          ];
+        } else {
+          // If the user typed letters, return school names in the autocomplete.
+          $matches[] = [
+            'value' => (string)$school_name[0]
+          ];
+        }
+      }
+    } else {
+      $matches[] = [
+        'value' => $this->t("No schools found")
+      ];
+    }
+    return $matches;
   }
 }
