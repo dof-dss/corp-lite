fddb66fb6d64cf3661cbf6248fa71e9bd641fe02
DuttonMa <duttonma@gmail.com>
2025-02-26 14:36:30 +0000

    Generate further code for School entity

---
+++ project/sites/etini/modules/custom/etini_district_inspectors/src/SchoolInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\etini_district_inspectors;
+
+use Drupal\Core\Entity\ContentEntityInterface;
+
+/**
+ * Provides an interface defining a school entity type.
+ */
+interface SchoolInterface extends ContentEntityInterface {
+
+}
