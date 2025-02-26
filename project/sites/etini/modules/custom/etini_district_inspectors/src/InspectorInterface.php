14661cf43c706033b353643bbe42989d91c9e109
DuttonMa <duttonma@gmail.com>
2025-02-26 14:48:10 +0000

    Add generated code for inspector entity

---
+++ project/sites/etini/modules/custom/etini_district_inspectors/src/InspectorInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\etini_district_inspectors;
+
+use Drupal\Core\Entity\ContentEntityInterface;
+
+/**
+ * Provides an interface defining an inspector entity type.
+ */
+interface InspectorInterface extends ContentEntityInterface {
+
+}
