ede64b1d355a230c5f6346ccf684e0d15858e40e
DuttonMa <duttonma@gmail.com>
2025-05-22 14:05:18 +0100

    Inspector ids caught in debug

--- project/sites/etini/modules/custom/etini_district_inspectors/src/Form/InspectorTransferForm.php
+++ project/sites/etini/modules/custom/etini_district_inspectors/src/Form/InspectorTransferForm.php
@@ -32,7 +32,7 @@ class InspectorTransferForm extends ConfigFormBase {
 
     $to_message = "Select the District Inspector taht you would like to transfer schools <b>to</b>.";
 
-    $form['site_url_list'] = [
+    $form['old_inspector_id'] = [
       '#type' => 'entity_autocomplete',
       '#title' => $this->t('District Inspector (old)'),
       '#target_type' => 'inspector',
@@ -47,7 +47,7 @@ class InspectorTransferForm extends ConfigFormBase {
       '#description' => $this->t($from_message)
     ];
 
-    $form['site_url_list_exclude'] = [
+    $form['new_inspector_id'] = [
       '#type' => 'entity_autocomplete',
       '#title' => $this->t('District Inspector (new)'),
       '#target_type' => 'inspector',
@@ -68,33 +68,14 @@ class InspectorTransferForm extends ConfigFormBase {
   /**
    * {@inheritdoc}
    */
-  public function validateForm(array &$form, FormStateInterface $form_state) {
-    $form_keys = ['site_url_list', 'site_url_list_exclude'];
-
-    foreach ($form_keys as $form_key) {
-      if ($form_state->getValue($form_key)) {
-        $replace_url_list = explode(PHP_EOL, $form_state->getValue($form_key));
-        foreach ($replace_url_list as $replace_url) {
-          // Make sure url is 'clean'.
-          $replace_url = str_replace(["\n", "\t", "\r"], '', $replace_url);
-          $pass = FALSE;
-          if (preg_match('|^http:\/\/|', $replace_url) || preg_match('|^https:\/\/|', $replace_url)) {
-            $pass = TRUE;
-          }
-          if (!$pass) {
-            $form_state->setErrorByName($form_key, $this->t("URLs must start with 'http' or 'https'"));
-          }
-        }
-      }
-    }
-    parent::validateForm($form, $form_state);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     parent::submitForm($form, $form_state);
+
+    $from_id = $form_state->getValue('old_inspector_id');
+    $to_id = $form_state->getValue('new_inspector_id');
+
+    $message = "From is $from_id , to is $to_id";
+    \Drupal::logger('etini_district_inspectors')->notice(t($message));
 
     /*$this->config('unity_internal_link_checker.linksettings')
       ->set('site_url_list', $form_state->getValue('site_url_list'))
