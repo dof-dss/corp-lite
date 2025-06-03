78030679100b3e06b293425062112b6ddb78dbf5
DuttonMa <duttonma@gmail.com>
2025-06-03 16:26:49 +0100

    Allow editing of inspector attached to school

--- project/sites/etini/modules/custom/etini_district_inspectors/src/Form/SchoolForm.php
+++ project/sites/etini/modules/custom/etini_district_inspectors/src/Form/SchoolForm.php
@@ -6,6 +6,7 @@ namespace Drupal\etini_district_inspectors\Form;
 
 use Drupal\Core\Entity\ContentEntityForm;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\etini_district_inspectors\Entity\School;
 
 /**
  * Form controller for the school entity edit forms.
@@ -16,30 +17,65 @@ final class SchoolForm extends ContentEntityForm {
    * {@inheritdoc}
    */
   public function save(array $form, FormStateInterface $form_state): int {
-    $result = parent::save($form, $form_state);
+    // Load up the school.
+    $entity = $this->buildEntity($form, $form_state);
+    $school_id = $entity->id();
 
-    $message_args = ['%label' => $this->entity->toLink()->toString()];
+    // Retrieve the values set in the form.
+    $inspector_id = $form_state->getValue('inspector_id');
+    $name = $form_state->getValue('Name')[0]['value'];
+    $de_reference = $form_state->getValue('de_reference')[0]['value'];
+    $phases = $form_state->getValue('phases')[0]['value'];
+
+    if (!empty($school_id)) {
+      // Edit the school.
+      $school = School::load($school_id);
+      $result = NULL;
+      if (!empty($school)) {
+        $revision = clone $school;
+        $revision->setNewRevision();
+        $revision->set('inspector_id', $inspector_id);
+        $revision->set('Name', $name);
+        $revision->set('de_reference', $de_reference);
+        $revision->set('phases', $phases);
+        $revision->setRevisionCreationTime(\Drupal::time()->getCurrentTime());
+        $revision->setRevisionUserId(\Drupal::currentuser()->id());
+        $result = $revision->save();
+      }
+    } else {
+      $school = School::create([
+        'inspector_id' => $inspector_id,
+        'Name' => $name,
+        'de_reference' => $de_reference,
+        'phases' => $phases
+      ]);
+      $school->save();
+      $school_id = $school->id();
+      $result = SAVED_NEW;
+    }
+
+
+    $message_args = ['%label' => $school->get('Name')->getString()];
     $logger_args = [
-      '%label' => $this->entity->label(),
-      'link' => $this->entity->toLink($this->t('View'))->toString(),
+      '%label' => $school->get('Name')->getString(),
+      '%inspector' => $inspector_id,
+      '%schoolid' => $school_id
     ];
 
     switch ($result) {
       case SAVED_NEW:
         $this->messenger()->addStatus($this->t('New school %label has been created.', $message_args));
-        $this->logger('etini_district_inspectors')->notice('New school %label has been created.', $logger_args);
+        $this->logger('etini_district_inspectors')->notice('New school %label has been created with id %schoolid.', $logger_args);
         break;
 
       case SAVED_UPDATED:
-        $this->messenger()->addStatus($this->t('The school %label has been updated.', $message_args));
-        $this->logger('etini_district_inspectors')->notice('The school %label has been updated.', $logger_args);
+        $this->messenger()->addStatus($this->t('The school id %label has been updated.', $message_args));
+        $this->logger('etini_district_inspectors')->notice('The school %label has been updated, inspector id is %inspector.', $logger_args);
         break;
 
       default:
         throw new \LogicException('Could not save the entity.');
     }
-
-    $form_state->setRedirectUrl($this->entity->toUrl());
 
     return $result;
   }
